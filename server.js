f4af7b2c5c96cca52c6acdabbd3bb3e8539adb8b
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-04-20 01:10:40 -0700

    new post functionality added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var blogDB = require('./DB.json');
+
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 var app = express();
+app.use(bodyParser.json());
 
-var people = ['John', 'Sara', 'Paul'];
 
 var blog = {
   siteTitle: "The Good Blog",
@@ -47,22 +52,61 @@ var blog = {
          ]
 };
 
+function setPostDate(blogObj) {
+  var d = new Date();
+  var prettyDate = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+  blogObj.lastUpdate = prettyDate;
+  blogObj.posts[blogObj.posts.length - 1].date = prettyDate;
+}
 
+
+function updateDB(blogObj, newPost) {
+  console.log("Running the updateDB funciton");
+  //var newObj = JSON.parse(blogObj);
+    blogObj.posts.push({title: newPost.titleText, body: newPost.bodyText});
+    setPostDate(blogObj);
+    blogObj.numberOfPosts = blogObj.posts.length;
+    //console.log(blogDB);
+    fs.writeFile('DB.json', JSON.stringify(blogObj), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+      // fs.readFile('DB.json', (err, data) => {
+      //     if (err) console.log("No databse");
+      //     blogDB = JSON.parse(data);
+      // });
+    });
+}
+
+app.post('/post', function (req, res){
+  if (req.body && req.body.titleText && req.body.bodyText) {
+    console.log("===== NEW POST =====");
+    console.log("  - title:", req.body.titleText);
+    console.log("  - body:", req.body.bodyText);
+    console.log("  - object:", req.body);
+    //console.log(blogDB);
+    updateDB(blogDB, req.body);
+
+    res.status(200).send("Photo successfully added");
+  } else {
+    res.status(400).send("Requests to this path must " +
+      "contain a JSON body with Title and Body " +
+      "fields.");
+  }
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 80));
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.render('home', {
 
       content: 'This is some content',
-      people: people,
       siteTitle: blog.siteTitle,
-      posts: blog.posts,
+      posts: blogDB.posts,
       //lastUpdate: date,
       //numberOfPosts: postCount,
       // lastUpdate: blog.lastUpdate(),
