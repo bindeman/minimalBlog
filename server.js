75b6a843a6a3482dcfbd6264b60cf82b06a13b4a
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-04-20 23:03:52 -0700

    css, html, and URL mapping fixes

--- server.js
+++ server.js
@@ -11,57 +11,59 @@ var app = express();
 app.use(bodyParser.json());
 
 
-var blog = {
-  siteTitle: "The Good Blog",
-  subtitle : "The Center for all your blog needs",
-  //lastUpdate: "No Recent Update",
-
-  numberOfPosts : function() {
-    return this.posts.length + " posts";
-  },
-
-  lastUpdate : function() {
-
-    return "Last Update: " + this.posts[this.posts.length - 1].date;
-  },
-  posts: [{title: "Drive",
-           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-           date: "Febraury 12, 2019",
-           id: 0
-            },
-          {title: "Deez Penuts",
-           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-           date: "November 1, 2019",
-           id: 1
-         },
-          {title: "Coronavirus helps with weight Loss, experts say",
-           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-           date: "Febraury 12, 2018",
-           id: 2
-         },
-         {title: "The world needs more leaders",
-          body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          date: "March 22, 2017",
-          id: 3
-        },
-        {title: "What is Love",
-         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-         date: "December 4, 2016",
-         id: 4
-          }
-         ]
-};
+// var blog = {
+//   siteTitle: "The Good Blog",
+//   subtitle : "The Center for all your blog needs",
+//   //lastUpdate: "No Recent Update",
+//
+//   numberOfPosts : function() {
+//     return this.posts.length + " posts";
+//   },
+//
+//   lastUpdate : function() {
+//
+//     return "Last Update: " + this.posts[this.posts.length - 1].date;
+//   },
+//   posts: [{title: "Drive",
+//            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//            date: "Febraury 12, 2019",
+//            id: 0
+//             },
+//           {title: "Deez Penuts",
+//            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//            date: "November 1, 2019",
+//            id: 1
+//          },
+//           {title: "Coronavirus helps with weight Loss, experts say",
+//            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//            date: "Febraury 12, 2018",
+//            id: 2
+//          },
+//          {title: "The world needs more leaders",
+//           body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//           date: "March 22, 2017",
+//           id: 3
+//         },
+//         {title: "What is Love",
+//          body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//          date: "December 4, 2016",
+//          id: 4
+//           }
+//          ]
+// };
 
 function setPostDate(blogObj) {
   var d = new Date();
   var prettyDate = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
   blogObj.lastUpdate = prettyDate;
   blogObj.posts[blogObj.posts.length - 1].date = prettyDate;
+  blogObj.posts[blogObj.posts.length - 1].id = blogObj.posts.length - 1;
+
 }
 
 
 function updateDB(blogObj, newPost) {
-  console.log("Running the updateDB funciton");
+  console.log("Running the updateDB function");
   //var newObj = JSON.parse(blogObj);
     blogObj.posts.push({title: newPost.titleText, body: newPost.bodyText});
     setPostDate(blogObj);
@@ -83,7 +85,6 @@ app.post('/post', function (req, res){
     console.log("  - title:", req.body.titleText);
     console.log("  - body:", req.body.bodyText);
     console.log("  - object:", req.body);
-    //console.log(blogDB);
     updateDB(blogDB, req.body);
 
     res.status(200).send("Photo successfully added");
@@ -105,17 +106,17 @@ app.get('/', function(req, res) {
     res.render('home', {
 
       content: 'This is some content',
-      siteTitle: blog.siteTitle,
+      siteTitle: blogDB.siteTitle,
       posts: blogDB.posts,
       //lastUpdate: date,
       //numberOfPosts: postCount,
       // lastUpdate: blog.lastUpdate(),
       helpers: {
             numberOfPosts: function () {
-              return blog.numberOfPosts();
+              return blogDB.numberOfPosts + " posts";
             },
             lastUpdate: function () {
-              return blog.lastUpdate();
+              return "Updated: " + blogDB.lastUpdate;
             }
 
 
@@ -131,24 +132,26 @@ app.get('/:postId', function (req, res, next) {
   // console.log(blog.posts[postId].title);
   // console.log(blog.posts[postId].date);
   // console.log(blog.posts[postId].body);
-  if (blog.posts[postId]) {
+  if (blogDB.posts[postId]) {
     // res.status(200).sendFile(
     //   __dirname + '/public/people/' + person + '.html'
     // );
     // console.log("== peopleData[person]:", peopleData[person])
     var relatedPostsArr = [];
     // function update() {
-      if(blog.posts[postId-1]) relatedPostsArr.push(blog.posts[postId-1]);
-      if(blog.posts[postId-2]) relatedPostsArr.push(blog.posts[postId-2]);
+      if(blogDB.posts[postId-1]) relatedPostsArr.push(Object.assign({}, blogDB.posts[postId-1]));
+      if(blogDB.posts[postId-2]) relatedPostsArr.push(Object.assign({}, blogDB.posts[postId-2]));
+      if(blogDB.posts[postId+1] && relatedPostsArr.length === 0) relatedPostsArr.push(Object.assign({}, blogDB.posts[postId+1]));
+      if(blogDB.posts[postId+2] && relatedPostsArr.length === 1) relatedPostsArr.push(Object.assign({}, blogDB.posts[postId+2]));
 
       //console.log(relatedPosts);
       relatedPostsArr.forEach(relateditem => relateditem.body = relateditem.body.slice(0, 150));
     // }
     res.render('single', {
-      title: blog.posts[postId].title,
-      date: blog.posts[postId].date,
-      body: blog.posts[postId].body,
-      siteTitle: blog.siteTitle,
+      title: blogDB.posts[postId].title,
+      date: blogDB.posts[postId].date,
+      body: blogDB.posts[postId].body,
+      siteTitle: blogDB.siteTitle,
       relatedPosts: relatedPostsArr
       // helpers: {
       //       relatedPosts: function () {
